3669524b533176f698dbea179c204009fda37492
lornxzxlXD <148486253+lornxzxlXD@users.noreply.github.com>
2024-05-20 09:00:48 -0600

    Create Ejercicio2.py

---
+++ Parcial1_2A/Ejercicio2.py
@@ -0,0 +1,8 @@
+#Crear un script que muestre en pantalla todos los numeros
+# pares del 1 al 20 
+# Usar un bucle for para iterar del 1 al 20
+for num in range(1, 21):
+    # Comprobar si el número es par
+    if num % 2 == 0:
+        # Mostrar el número en pantalla
+        print(num)

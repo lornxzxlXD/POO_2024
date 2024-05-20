8b853fe15fea972d5b96912442607920e904176c
lornxzxlXD <148486253+lornxzxlXD@users.noreply.github.com>
2024-05-20 09:00:22 -0600

    Create Ejercicio1.py

---
+++ Parcial1_2A/Ejercicio1.py
@@ -0,0 +1,14 @@
+ #Crear 2 variables una pais y otra continente 
+# Mostrar su valor en pantalla
+#Poner un comentario para poner el tipo de dato
+
+#Entrada 
+
+pais = "francia"  # tipo de dato: str (cadena de texto)
+
+# Crear la variable 'continente' y asignarle un valor
+continente = "Europa"  # tipo de dato: str (cadena de texto)
+
+# Mostrar el valor de las variables en pantalla
+print("País:", pais)
+print("Continente:", continente)
